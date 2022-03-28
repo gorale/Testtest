40df8187b8ef6d2e8a0d4bfe201a8cda5ea9451b
TigranTigranyan <T.tigranyan1993@mail.ru>
2022-03-28 15:53:22 +0400

    3

--- src/com/company/String.java
+++ src/com/company/String.java
@@ -4,5 +4,6 @@ public class String {
     java.lang.String a = "a";
     int b = 89;
     static int i = 8;
-    java.lang.String str="sdfgh";
+    java.lang.String str="sdf";
+    int anInt=12345;
 }
